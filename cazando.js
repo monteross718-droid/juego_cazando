bef95de40ea83b4f960f7dc30505191e5da80185
monteross718-droid <monteross718@gmail.com>
2026-06-28 13:38:35 -0500

    centrar posición gato y mover comida a esquina inferior derecha

--- cazando.js
+++ cazando.js
@@ -1,14 +1,31 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
-const ALTURA_SUELO=20;
-const ALTURA_PERSONAJE=60;
-const ANCHO_PERSONAJE=40;
+const ALTO_GATO=50;
+const ANCHO_GATO=50;
+const ALTO_COMIDA=50;
+const ANCHO_COMIDA=50;
 
+let gatoX = ANCHO_GATO;
+let gatoY= ALTO_GATO;
+let comidaX = ANCHO_COMIDA;
+let comidaY = ALTO_COMIDA;
 
+function iniciarJuego(){
+graficarGato();
+graficarComida();
+
+}
 
 function graficarGato(){
     ctx.fillStyle="blue";
-    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
+    ctx.fillRect(230,230,gatoX,gatoY);
+
+}
+
+function graficarComida(){
+
+    ctx.fillStyle="orange";
+    ctx.fillRect(450,450,comidaX,comidaY);
 
 }
